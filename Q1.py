be08ca5a0368e7809478a69005ea48c3fa20dcf7
YIHAN LI <liyihan@LIdeMacBook-Air.local>
2025-03-01 16:21:08 -0800

    finish q1

---
+++ Q1.py
@@ -0,0 +1,32 @@
+#def rollar_coaster_problem():
+ #   N = int(input())
+  #  C = int(input())
+   # P = int(input())
+
+#for i in range:
+ #   if 2*C*P <= N:
+  #      print (No) 
+   # else:
+    #     print (Yes)
+
+
+print("第一题:")
+
+import sys
+
+def roller_coaster_ride():
+    N = int(input())  
+    C = int(input())
+    P = int(input())
+    max_capacity = C * P  
+
+    if N <= max_capacity:
+        print("yes")
+    else:
+        print("no")
+if __name__ == "__main__":
+    roller_coaster_ride()
+
+
+print("第二题")
+
